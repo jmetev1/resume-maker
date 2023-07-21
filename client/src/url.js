98e01bf4ffd973b53ea9c21eba19f6e0aa8878ae
jacques metevier <jmetevier@gmail.com>
2023-07-21 15:26:50 -0700

    right url

--- client/src/url.js
+++ client/src/url.js
@@ -4,10 +4,9 @@ dotenv.load();
 
 // export const url = "http://pglebapp.mqhhuyuruj.us-east-2.elasticbeanstalk.com/"
 
-
-// export const url = 'https://goldfish-app-2-kbthc.ondigitalocean.app/'
-export const url = 'http://localhost:3001/api/'
-// export const url = '/api/'
+const dev = process.env.NODE_ENV;
+console.log({ dev })
+export const url = dev ? '/api/' : 'https://goldfish-app-2-kbthc.ondigitalocean.app/api/'
 
 // export const url = process.env.REACT_APP.local_host === 'true' ?
 // '/api/'
